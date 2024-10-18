b327da11dd5416a86eb3a08b26e89956da1cd590
dimmestsalmon <appelnephi@gmail.com>
2024-10-18 15:34:51 -0400

    Almost finished structuring shown data

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const getData = (inputValue) => {
   .then(jsonData => document.querySelector("body").innerHTML +=
     `<ul>
       <li>Name: ${jsonData.results[0].name}</li>
-    <ul>`
+      <li>Height: ${jsonData.results[0].height}</li>
+      <li>Eye: ${jsonData.results[0].eye_color}</li>`,
+        fetch (jsonData.results[0].homeworld)
+        .then(response => response.json())
+        .then(jsonData => document.querySelector("body").innerHTML +=
+      `<li>Home World: ${jsonData.results[0].name}</li>
+      </ul>`
+        )
     )
 }
 
